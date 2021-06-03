f94e0ed946c518f96674bd68bd0f507f598416ca
GuilleGorines <guillermo.gorines@gmail.com>
2021-06-03 19:18:13 +0200

    updated

---
+++ fastqc_directories.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# 1: directory containing the raw data (after Sample_catalog.py)
+DIRECTORY_input="$1"
+DIRECTORY=$(realpath $DIRECTORY_input)
+
+# 2: directory on which the full analysis is performed
+MOTHER_DIR_input="$2"
+MOTHER_DIR=$(realpath $MOTHER_DIR_input)
+
+# create trimmed folder if it doesnt exist
+if [ ! -d ${MOTHER_DIR}/TRIMMED ];
+then
+    mkdir ${MOTHER_DIR}/TRIMMED
+fi
+
+# create analysis folder if it doesnt exist
+if [ ! -d ${MOTHER_DIR}/ANALYSIS ];
+then
+    mkdir ${MOTHER_DIR}/ANALYSIS
+fi
+
+printf "sample-id\tforward-absolute-filepath\treverse-absolute-filepath\n" > ${MOTHER_DIR}/ANALYSIS/manifest.tsv
+
+# create results folder if it doesnt exist
+if [ ! -d ${MOTHER_DIR}/RESULTS ];
+then
+    mkdir ${MOTHER_DIR}/RESULTS
+fi
+
+for directory in $DIRECTORY/*/
+do
+    # samplename = dirname
+    samplename=$(basename $directory)
+    
+    # first quality result
+    cd $directory
+    mkdir ${MOTHER_DIR}/RESULTS/${samplename}_quality
+    fastqc -o ${MOTHER_DIR}/RESULTS/${samplename}_quality *.gz
+    
+    # perform trimming
+    fastp -i *R1* -I *R2* \
+    -o ${samplename}_trimmed_R1.fq.gz\
+    -O ${samplename}_trimmed_R2.fq.gz
+    
+
+    # second quality analysis
+    fastqc -o ${MOTHER_DIR}/RESULTS/${samplename}_quality *_trimmed_*
+    mv ${samplename}_trimmed_R1.fq.gz $MOTHER_DIR/TRIMMED
+    mv ${samplename}_trimmed_R2.fq.gz $MOTHER_DIR/TRIMMED
+
+    # add the data to the sample manifest
+    printf "${samplename}\t${MOTHER_DIR}/TRIMMED/${samplename}_trimmed_R1.fq.gz\t${MOTHER_DIR}/TRIMMED/${samplename}_trimmed_R2.fq.gz\n" >> ${MOTHER_DIR}/ANALYSIS/manifest.tsv
+
+done
