d5e38ec613fa61ce39297b71a3324c3e45df27f9
GuilleGorines <guillermo.gorines@gmail.com>
2021-11-06 23:02:34 +0100

    Update process_table.py

--- process_table.py
+++ process_table.py
@@ -1,49 +1,71 @@
 #!/usr/bin/env python
 import sys
 
-taxonomy = sys.argv[1]
-biom = sys.argv[2]
-outfile_name = sys.argv[3]
-
-# read taxonomy and extract first and second lines, ID and taxonomy, respectively
-with open(taxonomy, "r") as infile:
-    taxonomy = infile.readlines()
-    taxonomy = [line.replace("\n","").split("\t") for line in taxonomy[1:]]
-    ids_list = [line[0] for line in taxonomy]
+biom = sys.argv[1]
+outfile_name = sys.argv[2]
 
 # read the (readable) biom table, extract the header
+# organism: row, number of sublists
+# samples: columns, number of items in any sublist
 with open(biom, "r") as infile:
     biom = infile.readlines()
     biom = [line.replace("\n","").split("\t") for line in biom[1:]]
 
-    header = biom[0]
+initial_organisms = len(biom)-1
 
-# initialize the changed file
-absolute_feature_table = [header]
+print("------------- BEGIN -------------")
+print("Organism entries: ", initial_organisms)
+print("Sample number: ", len(biom[0])-1, "\n")
 
-# change the ids in the biom for the species in the taxonomy
-for line in biom[1:]:
-    print(line)
-    identification = line[0]
-    index_of_species = ids_list.index(identification)
-    # generate new line with index as name
+print("------------- DEDUPLICATION BEGIN -------------")
+# avoid identical taxids
+# 1-save the header with the sample names
+absolute_feature_table = [biom[0]]
 
-    taxonomic_name = taxonomy[index_of_species][1]
-
-    new_line = [taxonomic_name] + line[1:]
-    print(new_line)
-    absolute_feature_table.append(new_line)
-
-# to transpose it, we'll put it in a dict first
 absolute_feature_table_dict = {}
 
-for index in range(0,len(header)-1):
+for item in biom[1:]:
+    tax_name = item[0]
+    numbers = [float(number) for number in item[1:]]
+
+    if tax_name in absolute_feature_table_dict.keys():
+        print("Found duplicate identification: ", tax_name)
+        for index in range(0,len(numbers)-1):
+            absolute_feature_table_dict[tax_name][index] += numbers[index]
+    else:
+        absolute_feature_table_dict[tax_name] = numbers
+
+print("\n")
+# organism: number of keys in absolute_feature_table_dict -1, corresponding to the header presentation
+# samples: number of entries in any sublist of absolute_feature_table_dict values
+
+values_dict = [ item for item in absolute_feature_table_dict.values()]
+
+print("------------- DEDUPLICATION ENDED -------------")
+print("Organism entries: ", len(absolute_feature_table_dict.keys()))
+print("Sample number: ", len(values_dict[1]))
+duplicate_numbers = initial_organisms - len(absolute_feature_table_dict.keys())
+print(duplicate_numbers," duplicated organisms were found.\n")
+print("------------- END -------------\n")
+
+for key,values in absolute_feature_table_dict.items():
+
+    file_row = [key]
+    file_row.extend(values)
+    absolute_feature_table.append(file_row)
+
+# transpose it through dict
+absolute_feature_table_dict = {}
+
+for index in range(0,len(absolute_feature_table[0])):
     absolute_feature_table_dict[index]=[item[index] for item in absolute_feature_table]
 
-with open(outfile_name,"w") as outfile:
-    for item in absolute_feature_table_dict.values():
+absolute_feature_table = [item for item in absolute_feature_table_dict.values()]
 
-        line_to_write = "\t".join(item)
+with open(outfile_name,"w") as outfile:
+    for item in absolute_feature_table_dict.values():       
+
+        line_to_write = "\t".join([str(subitem) for subitem in item])
 
         outfile.write(line_to_write)
         outfile.write("\n")
