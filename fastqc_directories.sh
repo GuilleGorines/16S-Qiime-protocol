aa424930a251044d82f97ed209517135c4f3383b
GuilleGorines <guillermo.gorines@gmail.com>
2021-11-16 21:12:36 +0100

    Update fastqc_directories.sh

--- fastqc_directories.sh
+++ fastqc_directories.sh
@@ -1,65 +1,80 @@
 #!/bin/bash
 
-# 1: directory containing the raw data (after Sample_catalog.py) (typically RAW/)
+# 1: directory containing the raw data (after sample_catalog.py) (typically 00-reads/)
 DIRECTORY_input="$1"
 DIRECTORY=$(realpath $DIRECTORY_input)
 
-# 2: directory on which the full analysis is performed (typically .) 
-MOTHER_DIR_input="$2"
-MOTHER_DIR=$(realpath $MOTHER_DIR_input)
-
 # create trimmed folder if it doesnt exist
-if [ ! -d ${MOTHER_DIR}/TRIMMED ];
+if [ ! -d 01-quality_control ];
 then
-    mkdir ${MOTHER_DIR}/TRIMMED
+    mkdir 01-quality_control
+    echo "Created 01-quality_control directory."
+else
+    echo "Directory 01-quality_control already there."
 fi
-
-# create analysis folder if it doesnt exist
-if [ ! -d ${MOTHER_DIR}/ANALYSIS ];
-then
-    mkdir ${MOTHER_DIR}/ANALYSIS
-fi
-
-printf "sample-id\tforward-absolute-filepath\treverse-absolute-filepath\n" > ${MOTHER_DIR}/ANALYSIS/manifest.tsv
 
 # create results folder if it doesnt exist
-if [ ! -d ${MOTHER_DIR}/RESULTS ];
+
+if [ ! -d ../RESULTS ];
 then
-    mkdir ${MOTHER_DIR}/RESULTS
+    mkdir ../RESULTS
+    mkdir ../RESULTS/Quality_control
+    echo "Created RESULTS and RESULTS/Quality_control directories."
+else
+    if [ ! -d ../RESULTS/Quality_control]
+    then
+        mkdir ../RESULTS/Quality_control
+        echo "Created RESULTS/Quality_control directory."
+    else
+        echo "Directories RESULTS and RESULTS/Quality_control already there"
+    fi
 fi
+
+printf "sample-id\tforward-absolute-filepath\treverse-absolute-filepath\n" > manifest.tsv
+
+absolute_filepath = $(pwd)
 
 for directory in $DIRECTORY/*/
 do
+    echo "Starting ${directory} quality control."
     # samplename = dirname
     samplename=$(basename $directory)
     
-    # first quality result
-    cd $directory
-    mkdir ${MOTHER_DIR}/RESULTS/${samplename}_quality
-    fastqc -o ${MOTHER_DIR}/RESULTS/${samplename}_quality *.gz
-    
-    mkdir ${MOTHER_DIR}/RESULTS/${samplename}_trimming
-    # perform trimming
-    fastp -i *R1* -I *R2* \
+    # make the directoryfirst quality result
+    mkdir ../RESULTS/Quality_control/${samplename}_quality/fastqc_pre-trimming_reports
+    fastqc -o ../RESULTS/Quality_control/${samplename}_quality/fastqc_pre-trimming_reports $directory/*.gz
+       
+    # make the directories where the quality control reports 
+    # and the trimmed reads will be stored, and perform trimming
+    mkdir ../RESULTS/Quality_control/${samplename}_quality/fastp_reports
+    mkdir 01-quality_control/${samplename}
+
+    fastp -i $directory/*R1* -I $directory/*R2* \
     --trim_poly_x \
     --cut_front --cut_tail \
     --cut_mean_quality 20 \
     --length_required 250 \
     --qualified_quality_phred 30 \
     --cut_window_size 4 \
-    --html  ${MOTHER_DIR}/RESULTS/${samplename}_quality/${samplename}_trim_report_fastp.html \
-    --json  ${MOTHER_DIR}/RESULTS/${samplename}_quality/${samplename}_trim_report_fastp.json \
-    -o ${samplename}_trimmed_R1.fq.gz \
-    -O ${samplename}_trimmed_R2.fq.gz  
+    --html  ../RESULTS/Quality_control/${samplename}_quality/fastp_reports/${samplename}_trim_report_fastp.html \
+    --json  ../RESULTS/Quality_control/${samplename}_quality/fastp_reports/${samplename}_trim_report_fastp.json \
+    -o 01-quality_control/${samplename}/${samplename}_trimmed_R1.fq.gz \
+    -O 01-quality_control/${samplename}/${samplename}_trimmed_R2.fq.gz
 
     # second quality analysis
-    fastqc -o ${MOTHER_DIR}/RESULTS/${samplename}_quality *_trimmed_*
-    mv ${samplename}_trimmed_R1.fq.gz $MOTHER_DIR/TRIMMED
-    mv ${samplename}_trimmed_R2.fq.gz $MOTHER_DIR/TRIMMED
+    mkdir ../RESULTS/Quality_control/${samplename}_quality/fastqc_post-trimming
+    fastqc -o /RESULTS/${samplename}_quality/fastqc_post-trimming 01-quality_control/${samplename}/*_trimmed_*
     
     # add the data to the sample manifest
-    printf "${samplename}\t${MOTHER_DIR}/TRIMMED/${samplename}_trimmed_R1.fq.gz\t${MOTHER_DIR}/TRIMMED/${samplename}_trimmed_R2.fq.gz\n" >> ${MOTHER_DIR}/ANALYSIS/manifest.tsv
+
+    printf "${samplename}\t${absolute_filepath}/01-quality_control/${samplename}/${samplename}_trimmed_R1.fq.gz\t${absolute_filepath}/01-quality_control/${samplename}/${samplename}_trimmed_R2.fq.gz\n" >> manifest.tsv
+    echo "${directory} quality control finished."
+
 done
 
+echo "Launching MultiQC"
+
+
+echo "MultiQC ended"
 # Ready to launch multiqc on RESULTS (use the correct environment for it)
 # multiqc RESULTS/*
