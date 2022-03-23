edb8c127624a8535decdd2e9a7f6433017e5c2f2
GuilleGorines <guillermo.gorines@gmail.com>
2022-03-23 17:26:28 +0100

    created a qzv metadata extraction script

---
+++ Extract_metadata.py
@@ -0,0 +1,50 @@
+'''
+=============================================================
+HEADER
+=============================================================
+INSTITUTION: BU-ISCIII
+AUTHOR: Guillermo J. Gorines Cordero
+MAIL: guillermo.gorines@urjc.es
+VERSION: 1
+CREATED: 23-03-2022
+REVISED: 23-02-2022
+DESCRIPTION: 
+INPUT:
+OUTPUT:
+'''
+import shutil
+import os
+import sys
+
+import pandas as pd
+from qiime2 import Visualization
+
+filename = sys.argv[1] "collapsed_table_lvl6.qzv"
+outdir = sys.argv[2] "lvl6"
+
+table_name = f"{outdir}/data"
+
+# Open visualization
+qzv_file = Visualization.load(filename)
+
+# Export the data from the visualization onto a dir
+qzv_file.export_data(outdir)
+
+# Open the metadata tsv, remove the 
+# type of data (they are all numeric)
+# generate the long-table
+df = pd.read_csv(f"{outdir}/metadata.tsv", sep="\t", header=0, index_col=0)
+df = df.drop("#q2:types")
+df.to_csv(f"{table_name}.tsv", sep="\t")
+df.transpose().to_csv(f"{table_name}_long.tsv", sep="\t")
+
+# Delete unwanted dirs & files
+# Hardcoded but its always the same so
+dirs_to_del = ["css", "js", "q2templateassets"]
+
+for folder in dirs_to_del:
+    shutil.rmtree(f"{outdir}/{folder}")
+
+files_to_del = ["index.html", "metadata.tsv"]
+for file in files_to_del:
+    os.remove(f"{outdir}/{file}")
