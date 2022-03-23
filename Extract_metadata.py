ded946c3f05a3c4b552673277b60efae409d2ac9
GuilleGorines <guillermo.gorines@gmail.com>
2022-03-23 17:45:29 +0100

    updated metadata extraction script

--- Extract_metadata.py
+++ Extract_metadata.py
@@ -19,23 +19,24 @@ import sys
 import pandas as pd
 from qiime2 import Visualization
 
-filename = sys.argv[1] "collapsed_table_lvl6.qzv"
-outdir = sys.argv[2] "lvl6"
+infile = sys.argv[1] # "collapsed_table_lvl6.qzv"
+outdir = sys.argv[2] # "lvl6"
+outfile = sys.argv[3] # 
 
-table_name = f"{outdir}/data"
+table_name = f"{outdir}/{outfile}"
 
 # Open visualization
-qzv_file = Visualization.load(filename)
+qzv_file = Visualization.load(infile)
 
 # Export the data from the visualization onto a dir
 qzv_file.export_data(outdir)
 
 # Open the metadata tsv, remove the 
 # type of data (they are all numeric)
-# generate the long-table
+# generate the long-table (transposed)
 df = pd.read_csv(f"{outdir}/metadata.tsv", sep="\t", header=0, index_col=0)
 df = df.drop("#q2:types")
-df.to_csv(f"{table_name}.tsv", sep="\t")
+df.to_csv(f"{table_name}_wide.tsv", sep="\t")
 df.transpose().to_csv(f"{table_name}_long.tsv", sep="\t")
 
 # Delete unwanted dirs & files
