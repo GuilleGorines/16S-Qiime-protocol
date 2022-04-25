18677fe5d449c08556b9db5d463e586b51cbed90
GuilleGorines <guillermo.gorines@gmail.com>
2022-04-25 13:15:19 +0200

    dev phase completed maybe?

--- ANCOM_result_parse.py
+++ ANCOM_result_parse.py
@@ -1,17 +1,20 @@
 import argparse
 import uuid
 import os
+import sys
 import shutil
 import pandas as pd
 from qiime2 import Visualization
+from matplotlib.patches import Patch
+import matplotlib.pyplot as plt
 import seaborn as sns; sns.set_theme(color_codes=True)
 
-def save_long_wide(df, filename):
+def save_long_wide(df, filename, rowname, colname):
     """
     Generates tsv for a dataframe and for its transposed
     """
-    df.to_csv(f"{filename}_wide.tsv", sep="\t")
-    df.transpose().to_csv(f"{filename}_long.tsv", sep="\t")
+    df.to_csv(f"{filename}_row{rowname}_col{colname}.tsv", sep="\t")
+    df.transpose().to_csv(f"{filename}_row{colname}_col{rowname}.tsv", sep="\t")
     return
 
 def export_qzv(qzv_in, argument):
@@ -40,13 +43,14 @@ def export_qzv(qzv_in, argument):
     
     # remove the "w"
     df_ancom.drop(["W"], axis=1, inplace=True)
+    
     # Generate extra row to avoid NAs
     newrow = pd.DataFrame.from_dict({argument : ["-"]}, orient="index", columns=["Reject null hypothesis"])
     df_ancom = pd.concat([newrow, df_ancom], axis=0)
     
     # Percent abundances
     df_percent_abundances = pd.read_csv(f"{tmpdir}/percent-abundances.tsv", sep = "\t", index_col=0)
-    df_percent_abundances.rename({"Group" : argument},axis=0)
+    df_percent_abundances = df_percent_abundances.rename(index={"Group" : argument})
     
     shutil.rmtree(tmpdir)
 
@@ -71,7 +75,6 @@ parser = argparse.ArgumentParser(description='Generate the abundances and preval
 parser.add_argument('--qzv-in'         , help='feature table in qza format', dest="qzv_in", required=True)
 parser.add_argument('--metadata-file'  , help='metadata in tsv format', dest="metadata", required=True)
 parser.add_argument('--metadata-column', help='column to organize the metadata by', dest="metadata_column", required=True)
-parser.add_argument('--outdir'         , help='name of the output directory', dest="outdir", required=True)
 parser.add_argument('--mode'           , help='mode to choose from (full/filt)', choices= ["full", "filt"], dest="mode", required=True)
 parser.add_argument('--state'          , help='state to choose from', choices= ["raw", "clean"], dest="state", required=True)
 parser.add_argument('--level'          , help='level (for naming purposes)', dest="level", default=None)
@@ -80,19 +83,23 @@ args = parser.parse_args()
 
 df_ancom, df_data, df_percent_abundances = export_qzv(args.qzv_in, args.metadata_column)
 
-# generate first output
+# generate first file
+# Full ANCOM results: ancom data, percent_abundances
 df_out_1 = pd.concat([df_ancom, df_data, df_percent_abundances], axis=1)
-save_long_wide(df_out_1, "_")
+save_long_wide(df_out_1, f"lvl_{args.level}/{args.state}/complete_ancom_result_{args.metadata_column}_lvl_{args.level}_{args.state}_{args.mode}","taxa","ancom-full")
 
 # get the significative data 
 significative_taxa = get_significative_taxa(df_out_1)
 
 # import relative abundances
-rel_abs_df = pd.read_csv(rel_freq_in, header=0, index_col=0, delimiter="\t")
+rel_abs_df = pd.read_csv(args.relfreq_in, header=0, index_col=0, delimiter="\t")
 
 # Import metadata
 column_df = pd.read_csv(args.metadata, header=0, index_col=0, delimiter="\t").pop(args.metadata_column)
+
+# Second file generated: ancom with the relative frequence
 df_out_2 = pd.concat([df_ancom, df_data, pd.concat([pd.DataFrame(column_df).transpose(), rel_abs_df], axis=0)], axis=1)
+save_long_wide(df_out_2, f"lvl_{args.level}/{args.state}/ancom_result_w_rel_freq_{args.metadata_column}_lvl_{args.level}_{args.state}_{args.mode}", "taxa", "ancom-relfreq")
 
 # if there are any significative taxa
 # generate the heatmap with dendrogram plot
@@ -106,14 +113,14 @@ if significative_taxa is not None:
     # change current name into new future name
     # Get the values from the wanted columns
     newnames = [digest_name(item) for item in rownames]
-    newnames = [f"{item[-1]}; {item[-2]}" if "uncultured" in item[-1] and len(item) > 2 else item[-1] for item in newnames]
+    newnames = [f"{item[-1]}; {item[-2]}" if ("uncultured" in item[-1] and len(item) > 2) or ("__" in item[-1] and len(item) > 2) else item[-1] for item in newnames]
     
     namedict = { row : newname for row, newname in zip(rownames, newnames)}
     
     figure_df = sig_tax_abundances.rename(index=namedict)
     
     # associate color code to metadata
-    color_codes = dict(zip(column_df.unique(), ["green", "red", "blue", "purple", "grey"]))
+    color_codes = dict(zip(column_df.unique(), ["#00AA5A", "#C0AB52", "#e16a86",  "#00A6CA",  "#C699E7", "grey"]))
     col_colors = column_df.map(color_codes)
     
     figure = sns.clustermap(figure_df,
@@ -125,7 +132,11 @@ if significative_taxa is not None:
                   figsize=(15,10),
                   )
 
-    figure.savefig("hmap_xsamples_ytaxa.png")
+    handles = [Patch(facecolor=color_codes[name]) for name in color_codes]
+    plt.legend(handles, color_codes, title=args.metadata_column,
+           bbox_to_anchor=(1, 1), bbox_transform=plt.gcf().transFigure)
+
+    figure.savefig(f"lvl_{args.level}/{args.state}/hmap_{args.metadata_column}_lvl_{args.level}_{args.state}_{args.mode}_xsamples_ytaxa.png")
     
     reverse_figure_df = figure_df.transpose()
     
@@ -137,9 +148,29 @@ if significative_taxa is not None:
                             cmap="Greens",
                             figsize=(10,15),
                            )
+    plt.legend(handles, color_codes, title=args.metadata_column,
+        bbox_to_anchor=(1, 1), bbox_transform=plt.gcf().transFigure)
     
-    figure.savefig("hmap_xtaxa_ysamples.png")
-        
-    
+    figure.savefig(f"lvl_{args.level}/{args.state}/hmap_{args.metadata_column}_lvl_{args.level}_{args.state}_{args.mode}__xtaxa_ysamples.png")
 
+    # Third file
+    # Only significative taxa involved
+    relevant_rows = [args.metadata_column] + significative_taxa
+    df_out_3 = df_out_2.loc[relevant_rows, :]
+
+    # New df for Mode, level and state columns
+    newcols = pd.DataFrame([
+        ["-"] + [args.mode] * (df_out_3.shape[0]-1),
+        ["-"] + [args.level] * (df_out_3.shape[0]-1),
+        ["-"] + [args.state] * (df_out_3.shape[0]-1)
+        ]).transpose()
+    newcols.columns = ["Mode", "Level", "State"]
+    newcols.index = df_out_3.index
+
+    # Add the new columns
+    df_out_3 = pd.concat([newcols, df_out_3], axis=1)
+    save_long_wide(df_out_3, f"lvl_{args.level}/{args.state}/significative_results_{args.metadata_column}_lvl_{args.level}_{args.state}_{args.mode}", "taxa", "ancom-samples")
+
+else:
+    sys.exit(0)
 
